7f7347c4a1a7c95320cf63b0ebe3b2b2a13d548a
Luyang Li <liluyang@google.com>
2023-06-02 15:28:45 -0700

    .

--- coffee_calendar.py
+++ coffee_calendar.py
@@ -1,84 +1,118 @@
+import calendar
 import datetime
 import pytz
-from google.oauth2.credentials import Credentials
+from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 # Set the month for the table
-month = 4  # April
+month = 6
+month_name = calendar.month_abbr[month]
+year = datetime.datetime.now().year
 
 # Set up the Google Docs API client
-creds = Credentials.from_authorized_user_file('credentials.json', ['https://www.googleapis.com/auth/documents'])
-service = build('docs', 'v1', credentials=creds)
+credentials = service_account.Credentials.from_service_account_file('credentials.json', scopes=['https://www.googleapis.com/auth/documents'])
+service = build('docs', 'v1', credentials=credentials)
 
 # Create a new table
-table_rows = []
+dates = []
 for day in range(1, 32):
     try:
-        date = datetime.datetime(2023, month, day, tzinfo=pytz.UTC)
-        table_rows.append([date.strftime('%m/%d')] + [''] * 6)
+        date = datetime.datetime(year, month, day, tzinfo=pytz.UTC)
+        dates.append([date.strftime('%m/%d')] + [''] * 6)
     except ValueError:
         # Skip days that don't exist in the current month
         pass
     
 # Insert the table into the document
-document_id = 'DOCUMENT_ID_HERE'
+document_id = '1F13p5uxrfJnTDzITZGfBU5oBLGglMk5v8HGWYr2Ee20'
+rows = [
+    [
+        {
+            'text': '1'
+        },
+        {
+            'text': '2'
+        }
+    ],
+    [
+        {
+            'text': '3'
+        }
+    ]
+]
+columns = [
+    {
+        'text': '1'
+    },
+    {
+        'text': '2'
+    }
+]
+
 requests = [
     {
-        'createTable': {
-            'rowCount': len(table_rows),
-            'columnCount': 7,
-            'location': {
-                'index': 0
+        'insertTable': {
+            'rows': rows, #round(len(dates)/7),
+            'columns': columns,
+            'endOfSegmentLocation': {
+                'segmentId': ''
+                # 'index': 0
             }
         }
     },
     {
         'insertText': {
             'location': {
-                'index': 0
+                'index': 1
             },
-            'text': f'Monthly Table {month}\n'
-        }
-    },
-    {
-       'insertTableRows': {
-            'tableStartLocation': {
-                'index': 0
-            },
-            'insertBelow': False,
-            'number': len(table_rows)
-        }
-    },
-    {
-        'insertTableRow': {
-            'tableCellLocation': {
-                'tableStartLocation': {
-                    'index': 0
-                },
-                'rowIndex': 0,
-                'columnIndex': 0
-            },
-            'insertBelow': False,
-            'cells': [{'text': cell} for cell in ['Day'] + [''] * 6]
-        }
-    },
-    {
-        'insertText': {
-            'location': {
-                'index': 0
-            },
-            'text': '\n'
-        }
-    },
-    {
-        'insertText': {
-            'location': {
-                'index': 0
-            },
-            'text': '\n'.join(['\t'.join(row) for row in table_rows])
+            'text': f'{month_name}\n'
         }
     }
 ]
-service.documents().batchUpdate(documentId=document_id, body={'requests': requests}).execute()
 
-print(f'Table for month {month} inserted into document "{document_id}"')
+service.documents().batchUpdate(
+    documentId=document_id, 
+    body={
+        'requests': requests
+    }
+).execute()
+
+
+requests = [
+    {
+        'insertText': {
+            'location': {
+                'index': 6
+            },
+            'text': '06/01'
+        }
+    }
+]
+
+# days_in_month = calendar.monthrange(year, month)[1]
+# first_day = calendar.weekday(year, month, 1)
+#
+# for day in range(1, days_in_month + 1):
+#     day_of_week = (day - 1 + first_day) % 7
+#     table['rows'].append({
+#         'cells': [
+#             {
+#                 'text': '{:02d}'.format(day)
+#             }
+#         ] * day_of_week
+#     })
+#
+# requests = [{
+#     'insertTable': {
+#         'table': table
+#     }
+# }]
+
+service.documents().batchUpdate(
+    documentId=document_id, 
+    body={
+        'requests': requests
+    }
+).execute()
+
+# print(f'Table for month {month} inserted into document "{document_id}"')
